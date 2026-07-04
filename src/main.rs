096d10e78210e45c6327e41af6078d2429a8d0bb
Qther <qther@tuta.io>
2026-07-04 22:46:35 +0800

    ver: 1.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Result, bail};
 use base64::Engine;
+use bstr::ByteSlice;
 use miniserde::json::{Object, Value};
 use std::{
     io::Write,
@@ -8,39 +9,44 @@ use std::{
 
 fn main() -> Result<()> {
     let out = Command::new("ringboard").args(["debug", "dump"]).output()?;
-    let stdout = String::from_utf8(out.stdout)?;
+    let mut dump = if out.status.success() {
+        let stdout = String::from_utf8(out.stdout)?;
 
-    let dump: Vec<Object> = miniserde::json::from_str(&stdout)?;
-    let mut dump = dump
-        .into_iter()
-        .map(|obj| {
-            let mut data = None;
-            let mut id = None;
-            for (k, v) in obj.into_iter() {
-                if k == "id"
-                    && let Value::Number(n) = v
-                {
-                    id = Some(n);
-                } else if k == "data"
-                    && let Value::String(s) = v
-                {
-                    data = Some(s);
+        let dump: Vec<Object> = miniserde::json::from_str(&stdout)?;
+        let mut dump = dump
+            .into_iter()
+            .map(|obj| {
+                let mut data = None;
+                let mut id = None;
+                for (k, v) in obj.into_iter() {
+                    if k == "id"
+                        && let Value::Number(n) = v
+                    {
+                        id = Some(n);
+                    } else if k == "data"
+                        && let Value::String(s) = v
+                    {
+                        data = Some(s);
+                    }
                 }
-            }
 
-            let id = match id.expect("entries should all have an id") {
-                miniserde::json::Number::U64(id) => Ok(id),
-                miniserde::json::Number::I64(id) => id.try_into(),
-                miniserde::json::Number::F64(id) => unreachable!("id {id} should not be float"),
-            }
-            .expect("id should be valid");
+                let id = match id.expect("entries should all have an id") {
+                    miniserde::json::Number::U64(id) => Ok(id),
+                    miniserde::json::Number::I64(id) => id.try_into(),
+                    miniserde::json::Number::F64(id) => unreachable!("id {id} should not be float"),
+                }
+                .expect("id should be valid");
 
-            let data = data.expect("entries should all have data");
+                let data = data.expect("entries should all have data");
 
-            (id, data)
-        })
-        .collect::<Vec<_>>();
-    dump.sort_by_key(|e| !e.0);
+                (id, data)
+            })
+            .collect::<Vec<_>>();
+        dump.sort_by_key(|e| !e.0);
+        dump
+    } else {
+        Vec::new()
+    };
 
     let mut rofi = Command::new("rofi")
         .args(["-dmenu", "-sep", r"\0", "-p", "Ringboard", "-format", "i"])
@@ -53,7 +59,6 @@ fn main() -> Result<()> {
     let mut buf = Vec::new();
     for (_, data) in &dump {
         buf.clear();
-
         if base64::engine::general_purpose::STANDARD_NO_PAD
             .decode_vec(data, &mut buf)
             .is_ok()
@@ -64,15 +69,14 @@ fn main() -> Result<()> {
             continue;
         }
 
+        buf.clear();
         let bytes = data.as_bytes();
         let start = bytes
             .iter()
             .copied()
             .position(|b| !b.is_ascii_whitespace())
             .unwrap_or(0);
-
-        buf.clear();
-        buf.extend_from_slice(&bytes[start..]);
+        bytes[start..].replace_into(b"\n", b"\\n", &mut buf);
         buf.push(b'\0');
         stdin.write_all(&buf)?;
     }
